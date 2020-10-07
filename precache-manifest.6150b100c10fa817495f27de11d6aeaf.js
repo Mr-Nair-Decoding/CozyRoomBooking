b62a889c4b8fa808535bf85c5c28955722861d34
Mr-NairAtCoding <sachinnair16@gmail.com>
2020-10-07 16:47:55 +0530

    Updates

---
+++ precache-manifest.6150b100c10fa817495f27de11d6aeaf.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31a2ec00748b50ff6593c5ce36b86af",
+    "url": "/Mr-NairAtCoding/CozyRoomBooking/index.html"
+  },
+  {
+    "revision": "2a680ff7ed2c3e02851b",
+    "url": "/Mr-NairAtCoding/CozyRoomBooking/static/js/main.20307c73.chunk.js"
+  },
+  {
+    "revision": "961e01d37f2309bac10d",
+    "url": "/Mr-NairAtCoding/CozyRoomBooking/static/js/runtime-main.f86243da.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Mr-NairAtCoding/CozyRoomBookingprecache-manifest.e74dc41f218353f8c46773242411deab.js"
+  "/Mr-NairAtCoding/CozyRoomBooking/precache-manifest.6150b100c10fa817495f27de11d6aeaf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mr-NairAtCoding/CozyRoomBookingindex.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mr-NairAtCoding/CozyRoomBooking/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
