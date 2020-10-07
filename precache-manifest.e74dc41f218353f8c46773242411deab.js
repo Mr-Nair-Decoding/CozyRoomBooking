f6c37c34cb59b7b09101fe2d8a896f7819bad562
Mr-NairAtCoding <sachinnair16@gmail.com>
2020-10-07 16:45:17 +0530

    Updates

---
+++ precache-manifest.e74dc41f218353f8c46773242411deab.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de72397ce50251cba5aaf9b7f5fd352d",
+    "url": "/Mr-NairAtCoding/CozyRoomBookingindex.html"
+  },
+  {
+    "revision": "2a680ff7ed2c3e02851b",
+    "url": "/Mr-NairAtCoding/CozyRoomBookingstatic/js/main.20307c73.chunk.js"
+  },
+  {
+    "revision": "519e8d05e21c99b22032",
+    "url": "/Mr-NairAtCoding/CozyRoomBookingstatic/js/runtime-main.dea1b3ce.js"
+  }
+]);
---
+++ static/js/main.20307c73.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcozyroombookings=this.webpackJsonpcozyroombookings||[]).push([[0],[function(o,n,s){o.exports=s(1)},function(o,n){}],[[0,1]]]);
+//# sourceMappingURL=main.20307c73.chunk.js.map
